a8e9886187d3a74dbca90c1fd7f4be7d4b108077
lorenacastrorivera <lorenacastrorivera@gmail.com>
2026-06-18 12:48:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -162,29 +162,12 @@ if menu == "🔮 Simulación de Escenarios":
     )
     st.divider()
 
+    if menu == "🔮 Simulación de Escenarios":
+
+    ...
+
+    st.divider()
+
     if st.button("🚀 Generar Predicción"):
-
-        # modelo = joblib.load(
-        #     "modelo_regresion_lineal_bayesiana_multiple.pkl"
-        # )
-
-        entrada = pd.DataFrame({
-         "DECOS_PROM": [decos],
-         "MENSUALIDAD_PROM": [mensualidad],
-         "PROGRAMACION_MAS_FRECUENTE": [programacion],
-         "MOD_PAGO_MAS_FRECUENTE": [mod_pago],
-         "ESTADO_CUENTA_MAS_FRECUENTE": [estado],
-         "CLIENTES_LAG1": [lag1],
-         "CLIENTES_LAG3": [lag3],
-         "CLIENTES_LAG6": [lag6],
-         "CLIENTES_ROLLING3": [rolling3]
-        })
-
-        # prediccion = modelo.predict(entrada)[0]
-
         st.success("Predicción ejecutada correctamente.")
-
-        st.metric(
-         "Clientes Predichos",
-         21
-        )
+        st.metric("Clientes Predichos", 21)
