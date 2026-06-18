e3c3ba736833c827502449ad7b14a733b8b99fe4
lorenacastrorivera <lorenacastrorivera@gmail.com>
2026-06-18 15:35:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit as st
 import pandas as pd
 import pandas as pd
-import joblib
+
 # ==================================
 # CONFIGURACIÓN
 # ==================================
