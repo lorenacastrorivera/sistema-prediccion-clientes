39b5da752c6fffd1f577f8ee4a769f99d6bc9f93
lorenacastrorivera <lorenacastrorivera@gmail.com>
2026-08-26 20:18:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if menu == "🏠 Inicio":
 
     col1, col2, col3 = st.columns(3)
 
-    col1.metric("Observaciones", "190")
+    col1.metric("Observaciones", "184")
     col2.metric("Modelos Evaluados", "7")
     col3.metric("Mejor Modelo", "Bayesiana")
 
